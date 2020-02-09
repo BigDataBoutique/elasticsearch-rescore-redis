8a035ea07f0704929cc91543a95bbdc9c4849d35
Igor Piddubnyi <igor.piddubnyi@solvians.com>
2020-02-09 21:33:38 +0100

    Use container with redis to run integration test

--- src/test/java/com/bigdataboutique/elasticsearch/plugin/RedisRescoreClientYamlTestSuiteIT.java
+++ src/test/java/com/bigdataboutique/elasticsearch/plugin/RedisRescoreClientYamlTestSuiteIT.java
@@ -4,8 +4,18 @@ import com.carrotsearch.randomizedtesting.annotations.Name;
 import com.carrotsearch.randomizedtesting.annotations.ParametersFactory;
 import org.elasticsearch.test.rest.yaml.ClientYamlTestCandidate;
 import org.elasticsearch.test.rest.yaml.ESClientYamlSuiteTestCase;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 
 public class RedisRescoreClientYamlTestSuiteIT extends ESClientYamlSuiteTestCase {
+
+    @SuppressWarnings("rawtypes")
+    public static GenericContainer redis;
 
     public RedisRescoreClientYamlTestSuiteIT(@Name("yaml") ClientYamlTestCandidate testCandidate) {
         super(testCandidate);
@@ -14,5 +24,22 @@ public class RedisRescoreClientYamlTestSuiteIT extends ESClientYamlSuiteTestCase
     @ParametersFactory
     public static Iterable<Object[]> parameters() throws Exception {
         return ESClientYamlSuiteTestCase.createParameters();
+    }
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        AccessController.doPrivileged((PrivilegedAction<Void>) () -> {
+            redis = new GenericContainer<>("redis:3.2.0").withNetworkMode("host");
+            redis.start();
+            return null;
+        });
+    }
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        AccessController.doPrivileged((PrivilegedAction<Void>) () -> {
+            redis.stop();
+            return null;
+        });
     }
 }
